aa6d014cc644dee88504aa28ceaee920f9ba9ecb
AdrielBori <103082801+AdrielBori@users.noreply.github.com>
2022-09-22 23:28:48 -0300

    nuevos header y footer movil

--- src/script/index.js
+++ src/script/index.js
@@ -3,7 +3,7 @@ document.getElementById("burger__menu").addEventListener("click",()=>{
     const menudesplegable=document.getElementById("slide__menu")
     const menu=document.getElementById("burger__menu")
     const close=document.getElementById("close__menu")
-    menudesplegable.style.display="block"
+    menudesplegable.style.left="0"
     menu.style.display="none"
     close.style.display="block"
 })
@@ -11,7 +11,7 @@ document.getElementById("close__menu").addEventListener("click",()=>{
     const menudesplegable=document.getElementById("slide__menu")
     const menu=document.getElementById("burger__menu")
     const close=document.getElementById("close__menu")
-    menudesplegable.style.display="none"
+    menudesplegable.style.left="-100%"
     menu.style.display="block"
     close.style.display="none"
 })
