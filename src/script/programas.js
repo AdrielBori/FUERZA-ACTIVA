16747f72b609d07e09bd246c8e7992705789a455
AdrielBori <103082801+AdrielBori@users.noreply.github.com>
2022-09-28 15:19:05 -0300

    correccion detalles

--- src/script/programas.js
+++ src/script/programas.js
@@ -1,11 +1,29 @@
-let pixel=0
-document.getElementById("der").addEventListener("click", ()=>{
-    const card=document.getElementById("content__cards");
-    card.style.left=`${pixel+200}px`;
-    pixel=pixel+200;
+let rotar=1
+
+document.getElementById("flechader").addEventListener("click",()=>{
+    if(rotar==6){
+        rotar=1
+        let item="item-"+rotar
+        const Item=document.getElementById(item)
+        Item.checked=true
+
+    }else{
+        rotar++;
+        let item="item-"+rotar
+        const Item=document.getElementById(item)
+        Item.checked=true
+    }
 })
-document.getElementById("izq").addEventListener("click", ()=>{
-    const card=document.getElementById("content__cards");
-    card.style.left=`${pixel-200}px`;
-    pixel=pixel-200;
+document.getElementById("flechaizq").addEventListener("click",()=>{
+    if(rotar==1){
+        rotar=6
+        let item="item-"+rotar
+        const ItemR=document.getElementById(item)
+        ItemR.checked=true
+    }else{
+        rotar--;
+        let item="item-"+rotar
+        const ItemR=document.getElementById(item)
+        ItemR.checked=true
+    }
 })
